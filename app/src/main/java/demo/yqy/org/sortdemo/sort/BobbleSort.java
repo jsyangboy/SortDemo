efaadb1517fb5a13b6858e41f70028b84c64e2c1
yqy <tpyangqingyuan@163.com>
2019-07-09 23:03:35 +0800

    冒泡排序

--- app/src/main/java/demo/yqy/org/sortdemo/sort/BobbleSort.java
+++ app/src/main/java/demo/yqy/org/sortdemo/sort/BobbleSort.java
@@ -1,10 +1,73 @@
 package demo.yqy.org.sortdemo.sort;
 
+import java.util.Random;
+
 /**
  * @author yqy
  * @create 19-7-9
- * @Describe
+ * @Describe 冒泡排序
  */
 public class BobbleSort {
+
+
+    public static int[] bubbleSort(int arr[]) {
+        if (arr == null) {
+            return null;
+        }
+        if (arr.length <= 0) {
+            return null;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+
+            /**
+             * （arr.length - i - 1 ）：j要不断的往前移，
+             */
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                /**
+                 * 相邻之间相互比较
+                 */
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+    public static int[] arr = new int[20];
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+
+        /**
+         * 生成随机数
+         */
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100);
+        }
+
+        prinArr(arr);
+        bubbleSort(arr);
+        System.out.println("");
+        prinArr(arr);
+    }
+
+    private static void prinArr(int arr[]) {
+        if (arr == null) {
+            return;
+        }
+        if (arr.length <= 0) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("-" + arr[i]);
+        }
+    }
 
 }
